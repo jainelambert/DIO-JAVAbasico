b39ecc535f5e8557873c236e114660aac0f38ae2
Jaine <jainelambert@gmail.com>
2023-01-06 15:41:44 -0300

    aula tipos e variaveis java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
+//tipos primitivos
+//estudem a classe String que representa texto na aplicação
 public class TiposVariaveis{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short numeroCurto = 1;
+        System.out.println("Numero Short: " + numeroCurto);
+        int numeroNormal = (int) numeroCurto;
+        System.out.println("Numero int: " + numeroNormal);
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println("Numero Short2: " + numeroCurto2);
+
+        String meuNome = "Jaine Lambert";
+        System.out.println("Nome: " + meuNome);
+
+        final double NUMERO = 10;//constante
+        System.out.println("CONSTANTE: " + NUMERO);
+
     }
 }
